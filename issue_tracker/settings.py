4551164d46c6cba0172f6edcc22b190e43fc5401
Shaun Nugent <shaun.n88@gmail.com>
2018-09-24 12:27:36 +0000

    removed env for heroku

--- issue_tracker/settings.py
+++ issue_tracker/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-import env
+# import env
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
