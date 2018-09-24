594dafa93e9a70f0d718adee23f0c4db594dc434
Shaun Nugent <shaun.n88@gmail.com>
2018-09-24 11:56:57 +0000

    Changed Allowed host for Heroku

--- issue_tracker/settings.py
+++ issue_tracker/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.getenv('C9_HOSTNAME')]
+ALLOWED_HOSTS = [os.getenv('C9_HOSTNAME'), 'unicorn-issue-tracker.herokuapp.com']
 
 
 # Application definition
